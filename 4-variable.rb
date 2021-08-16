f4ad7ef6095304c54950e3ac3b85ce461bda490a
yingbo5 <yingbo5@staff.weibo.com>
2021-08-16 18:28:12 +0800

    variable

--- 4-variable.rb
+++ 4-variable.rb
@@ -23,6 +23,59 @@ puts "这是主 Ruby 程序"
 # https://www.runoob.com/ruby/ruby-variable.html
 puts "-------------开始-------------\n"
 
+=begin
+Ruby 支持五种类型的变量。
+
+一般小写字母、下划线开头：变量（Variable）。
+$开头：全局变量（Global variable）。
+@开头：实例变量（Instance variable）。
+@@开头：类变量（Class variable）类变量被共享在整个继承链中
+大写字母开头：常数（Constant）   
+=end
+
+
+=begin
+Ruby 伪变量
+它们是特殊的变量，有着局部变量的外观，但行为却像常量。您不能给这些变量赋任何值。
+
+self: 当前方法的接收器对象。
+true: 代表 true 的值。
+false: 代表 false 的值。
+nil: 代表 undefined 的值。
+__FILE__: 当前源文件的名称。
+__LINE__: 当前行在源文件中的编号。
+=end
+
+$global_variable = 10
+class Class1
+  def print_global
+      puts "全局变量在 Class1 中输出为 #$global_variable"
+  end
+end
+class Class2
+  def print_global
+      puts "全局变量在 Class2 中输出为 #$global_variable"
+  end
+end
+ 
+class1obj = Class1.new
+class1obj.print_global
+class2obj = Class2.new
+class2obj.print_global
+
+class Example
+   VAR1 = 100
+   VAR2 = 200
+   def show
+       puts "第一个常量的值为 #{VAR1}"
+       puts "第二个常量的值为 #{VAR2}"
+   end
+end
+ 
+# 创建对象
+object=Example.new()
+object.show
+
 
 
 #声明 code 会在程序的结尾被调用。
---
+++ 5-operator.rb
@@ -0,0 +1,65 @@
+#!/usr/bin/ruby
+# -*- coding: UTF-8 -*-
+
+
+
+
+# 声明 code 会在程序运行之前被调用 
+BEGIN {
+   puts "初始化 Ruby 程序"
+}
+
+
+
+puts "这是主 Ruby 程序"
+
+=begin
+这是注释。
+这也是注释。
+这也是注释。
+这还是注释。
+=end
+
+# https://www.runoob.com/ruby/ruby-operator.html
+puts "-------------开始-------------\n"
+puts "-------------Ruby defined? 运算符-------------\n"
+
+puts "-------------Ruby 点运算符 \".\" 和双冒号运算符 \"::\"-------------\n"
+
+
+puts "第一个实例"
+MR_COUNT = 0        # 定义在主 Object 类上的常量
+module Foo
+  MR_COUNT = 0
+  ::MR_COUNT = 1    # 设置全局计数为 1
+  MR_COUNT = 2      # 设置局部计数为 2
+end
+puts MR_COUNT       # 这是全局常量
+puts Foo::MR_COUNT  # 这是 "Foo" 的局部常量
+
+puts "第二个实例"
+CONST = ' out there'
+class Inside_one
+   CONST = proc {' in there'}
+   def where_is_my_CONST
+      ::CONST + ' inside one'
+   end
+end
+class Inside_two
+   CONST = ' inside two'
+   def where_is_my_CONST
+      CONST
+   end
+end
+puts Inside_one.new.where_is_my_CONST
+puts Inside_two.new.where_is_my_CONST
+puts Object::CONST + Inside_two::CONST
+puts Inside_two::CONST + CONST
+puts Inside_one::CONST
+puts Inside_one::CONST.call + Inside_two::CONST
+
+
+#声明 code 会在程序的结尾被调用。
+END {
+   puts "停止 Ruby 程序"
+}
